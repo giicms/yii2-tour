d2d274dde2a6f3783e4f1e2ee1cdb1b5c17579aa
Vinh Huynh <giicmsvn@gmail.com>
2016-06-20 20:28:10 +0700

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+namespace giicms\tour;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        // custom initialization code goes here
+    }
+}
